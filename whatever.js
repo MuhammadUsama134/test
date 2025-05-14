576fd285c3a9fbd60cf75db6eb8f94d4aa5e06b6
Muhammad Usama <www.usamaet134@gmail.com>
2025-05-14 14:00:00 +0500

    whatever

---
+++ whatever.js
@@ -0,0 +1,63 @@
+class name {
+    constructor(parameters) {
+        for(let a=1;a<6;a++)
+        {
+            if(a==5)
+            {
+                break;
+            }
+        }
+        console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;.");
+         console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;.");
+         console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;.");
+         console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;.");
+         console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;."); console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;."); console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;."); console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;."); console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;."); console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;."); console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;."); console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;."); console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;."); console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;."); console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;."); console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;."); console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;."); console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;."); console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;."); console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;."); console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;."); console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;.");
+        console.log("dfghjklgfdghvjhjknkm,l;.");
+
+
+        console.log("a")
+    }
+}
