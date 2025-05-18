d82173fb487f5fdada190b97872d830e1ecf33d3
Muhammad Usama <www.usamaet134@gmail.com>
2025-05-18 11:00:00 +0500

    JS file update

--- whatever.js
+++ whatever.js
@@ -1,5 +1,12 @@
 class name {
     constructor(parameters) {
+        for(let a=1;a<6;a++)
+        {
+            if(a==5)
+            {
+                break;
+            }
+        }
         
     }
 }
