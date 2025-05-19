f2f3b9dff202e63d5f96157b89446fd0ecc4bf02
Muhammad Usama <www.usamaet134@gmail.com>
2025-05-19 14:00:00 +0500

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,1 +1,24 @@
+#include<iostream>
+using namespace std;
+
+void whatever(char a)
+{
+for(int i=0;i<100;i++)
+  {
+    cout<<a<<endl;
+  }
+  
+}
+int main()
+{
+  char what[100]="drfghjkl;lkjhgfdsdfghjl;'l;khjfgdsdfghjm,.';hgdsaedfghjkl;;'lkjhgufdsdfgjkl
+  ';jghfd";
+for(int i=0;i<100;i++)
+  {
+whatever(what[i]);
+    
+  }
+
 cout<<"whatever"
+  cout<<"hehe";
+}
